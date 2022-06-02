1b251ee02a449827be9541badd82c696b8ee5e99
kailanajt <kailana.thompson12@gmail.com>
2022-06-02 16:53:22 -0500

    set constants and variables

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
-
+const players = {
+    '1' : {
+      letter: 'X'
+},
+'-1' : {
+      letter: 'O'
+    }
+}
 /*---------------------------- Variables (state) ----------------------------*/
-
+let board = ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null']
+let turn = [players[1], players[-1]]
+let winner = ['Congratulations! Letter '+ players.letter + ' has won the round!', 'Looks like a tie! Nobody won... bummer.']
+console.log(winner[0])
+console.log(winner[1])
 
 
 /*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelector('.squares')
+const messageEl = 'awaiting play...'
+console.log(messageEl)
 
-
+function init() {
+    return init
+}
+init()
+console.log(init)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
