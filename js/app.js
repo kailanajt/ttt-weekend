5310c72377b93d027c243f66858017423cab01a0
kailanajt <kailana.thompson12@gmail.com>
2022-06-05 20:54:18 -0500

    completed tic tac toe

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 /*-------------------------------- Constants --------------------------------*/
 const winningCombos = [
-    [0,1,2], [3,4,5], [6,7,8], 
-    [1,4,7], [0,3,6], [0,4,8],
+    [0,1,2], [3,4,5], 
+    [6,7,8], [1,4,7], 
+    [0,3,6], [0,4,8],
     [2,5,8], [2,4,6]
 
 ]
 /*---------------------------- Variables (state) ----------------------------*/
+
 let board, turn, winner
+
 /*------------------------ Cached Element References ------------------------*/
+
 const squareEls = document.querySelectorAll('.squares')
-// const boardEls = document.querySelectorAll('.board')
 const messageEl = document.getElementById('messages')
 const resetBtnEl = document.querySelector('#reset-button')
-
-
-
 
 /*----------------------------- Event Listeners -----------------------------*/
 //ternery style ex for later(?): evt.target.style.color === 'black' ? 'lime' : 'black'
